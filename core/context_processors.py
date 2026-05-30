jupyterhealth/jupyterhealth-exchange

commit ccb03b12958caaaf4efe8da8234b64b940ed570e

fix lookup of admin ui application (#451)
first() doesn't raise when none is found, it returns None

core/context_processors.py
@@ -14,13 +14,15 @@ logger = logging.getLogger(__name__)
 
 @lru_cache(maxsize=1)
 def _get_oidc_client_id():
+    Application = get_application_model()
     try:
-        Application = get_application_model()
-        return Application.objects.filter(name="JHE Admin UI").values_list("client_id", flat=True).first()
-    except Exception as exc:
-        logger.warning(
-            "Unable to load the OAuth2 client ID for 'JHE Admin UI' from the database: %s",
-            exc,
+        client_id = Application.objects.filter(name="JHE Admin UI").values_list("client_id", flat=True).first()
+    except Exception:
+        logger.exception("Error looking up OAuth2 client ID for 'JHE Admin UI'")
+        client_id = None
+    if client_id is None:
+        logger.error(
+            "Unable to load the OAuth2 client ID for 'JHE Admin UI' from the database. Make sure it is defined in Applications via the django admin interface (/admin/)."
         )
 
 

commit 4ddd5c8975b2015ae970913054dabe055455e297

fix: return client_id from _get_oidc_client_id (regression from #451)

core/context_processors.py
@@ -24,6 +24,7 @@ def _get_oidc_client_id():
         logger.error(
             "Unable to load the OAuth2 client ID for 'JHE Admin UI' from the database. Make sure it is defined in Applications via the django admin interface (/admin/)."
         )
+    return client_id
 
 
 def constants(request):